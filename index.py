f43135330e396f808f5f5c68c88ec33dca28873d
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2023-09-30 21:18:26 -0300

    boiler plate

---
+++ index.py
@@ -0,0 +1,57 @@
+import socket,warnings
+try:
+    socket.setdefaulttimeout(23)
+    socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(('1.1.1.1', 53))
+except socket.error as ex: raise Exception("STOP: No internet. Click '>|' in top right and set 'Internet' switch to on")
+
+import os
+
+from duckduckgo_search import ddg_images
+from fastcore.all import *
+
+def search_images(term, max_images=30):
+    print(f"Searching for '{term}'")
+    return L(ddg_images(term, max_results=max_images)).itemgot('image')
+
+urls = search_images('bird photos', max_images=1)
+urls[0]
+
+from fastdownload import download_url
+dest = 'bird.jpg'
+download_url(urls[0], dest, show_progress=False)
+
+from fastai.vision.all import *
+im = Image.open(dest)
+im.to_thumb(256,256)
+
+download_url(search_images('forest photos', max_images=1)[0], 'forest.jpg', show_progress=False)
+Image.open('forest.jpg').to_thumb(256,256)
+
+searches = 'forest','bird'
+path = Path('bird_or_not')
+from time import sleep
+
+for o in searches:
+    dest = (path/o)
+    dest.mkdir(exist_ok=True, parents=True)
+    download_images(dest, urls=search_images(f'{o} photo'))
+    sleep(10)  # Pause between searches to avoid over-loading server
+    download_images(dest, urls=search_images(f'{o} sun photo'))
+    sleep(10)
+    download_images(dest, urls=search_images(f'{o} shade photo'))
+    sleep(10)
+    resize_images(path/o, max_size=400, dest=path/o)
+    
+failed = verify_images(get_image_files(path))
+failed.map(Path.unlink)
+len(failed)
+
+dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock),
+    get_items=get_image_files,
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=[Resize(192, method='squish')]
+).dataloaders(path, bs=32)
+
+dls.show_batch(max_n=6)
